a560911be531bfe6bcc4449738dcf58b2e0e0192
Serwes Bhatta <serwesbhatta@Serwess-MBP.lan>
2024-12-06 12:41:54 -0600

    feat: add flags to grep

--- p01/shell/cmd_pkg/grep.py
+++ p01/shell/cmd_pkg/grep.py
@@ -1,23 +1,42 @@
 from .call_api import call_api
 from .fs_state_manager import Fs_state_manager
+from .get_flags import get_flags
 import re
 
+
 def grep(params=None, input=None):
-    if (params == None and input == None):
+    if params == None and input == None:
         return {
             "status": "fail",
-            "message": "\nPlease specify the pattern and the filename"
+            "message": "\nPlease specify the pattern and the filename",
         }
     elif len(params) == 1 and input == None:
         return {
             "status": "fail",
-            "message": "\nPlease specify the pattern first and file after that."
+            "message": "\nPlease specify the pattern first and file after that.",
         }
     elif input is not None and len(params) == 0:
-        return {
-            "status": "fail",
-            "message": "\nPlease specify the pattern"
-        }
+        return {"status": "fail", "message": "\nPlease specify the pattern"}
+
+    l_flag = False
+    i_flag = False
+    c_flag = False
+
+    allowed_flags = ["l", "i", "c"]
+    flags_response = get_flags(allowed_flags, params)
+    flags = flags_response["flags"]
+
+    if flags_response["invalid_flags"]:
+        return {"status": "fail", "message": "\nOnvalid flags"}
+
+    if flags:
+        if "l" in flags:
+            l_flag = True
+        if "i" in flags:
+            i_flag = True
+        if "c" in flags:
+            c_flag = True
+        params = [param for param in params if not param.startswith("-")]
 
     pattern = params[0]
     params.pop(0)
@@ -30,6 +49,8 @@ def grep(params=None, input=None):
 
     contents = ""
     message = ""
+    match_files = []
+    counts = {}
 
     if input == None:
         params = [element.rstrip(",") for element in params]
@@ -44,30 +65,59 @@ def grep(params=None, input=None):
                 response = call_api("files", params=filters)
 
                 if response["status"] == "success":
-                    contents += response["message"][0]["contents"] + "\n"
+                    contents = response["message"][0]["contents"] + "\n"
+                    lines = contents.split("\n")
+                    filename_added = False
+
+                    for line in lines:
+                        result = ""
+                        if i_flag:
+                            result = bool(re.search(pattern, line, re.IGNORECASE))
+                        else:
+                            result = bool(re.search(pattern, line))
+
+                        if result == True:
+                            if not filename_added:
+                                match_files.append(param)
+                                counts[param] = 0
+                                filename_added = True
+                            counts[param] += 1
+                            message += "\n" + line
 
                 else:
                     return {
                         "status": "fail",
-                        "message": f"\nFile '{filename}' doesn't exist."
+                        "message": f"\nFile '{filename}' doesn't exist.",
                     }
             except:
-                return {
-                    "status": "fail",
-                    "message": "\nCannot make a call to api."
-                }
+                return {"status": "fail", "message": "\nCannot make a call to api."}
     else:
         contents = input["message"]
 
-    lines = contents.split("\n")
+        lines = contents.split("\n")
 
-    for line in lines:
-        result = bool(re.search(pattern, line))
+        for line in lines:
+            result = ""
+            if i_flag:
+                result = bool(re.search(pattern, line, re.IGNORECASE))
+            else:
+                result = bool(re.search(pattern, line))
+            if result == True:
+                counts += 1
+                message += "\n" + line
 
-        if result == True:
-            message += "\n"+line
+    final_message = ""
 
-    return {
-        "status": "success",
-        "message": message
-    }
+    if flags != []:
+        if l_flag:
+            final_message += "\n" + "\n".join(match_files)
+        if c_flag:
+            if len(counts) > 1:
+                for key, value in counts.items():
+                    final_message += f"\n{key} : {value}"
+            elif len(params) == 1 and params[0] in counts:
+                final_message += f"\n{params[0]} : {counts[params[0]]}"
+    else:
+        final_message = message
+
+    return {"status": "success", "message": final_message}
